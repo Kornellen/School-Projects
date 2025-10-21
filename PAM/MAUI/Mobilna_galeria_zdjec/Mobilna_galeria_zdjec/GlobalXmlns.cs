c7955db6635fa8756d588de73dd14ac72fe99fcc
Kornellen <kornel.konopka@onet.pl>
2025-10-21 20:17:09 +0200

    Added branch Programowanie Aplikacji Desktopowych (PAD)

--- PAM/MAUI/Mobilna_galeria_zdjec/Mobilna_galeria_zdjec/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Mobilna_galeria_zdjec")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Mobilna_galeria_zdjec.Pages")]
