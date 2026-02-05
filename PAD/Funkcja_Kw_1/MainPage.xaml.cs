10aa3a17598afd09607d5deb8c6922f1ebc7f25d
Kornellen <kornel.konopka@onet.pl>
2026-02-05 21:33:12 +0100

    Added Funkcja_Kw_1

---
+++ PAD/Funkcja_Kw_1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Funkcja_Kw_1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private double CalculateQuadric(double x) => 3 * Math.Pow(x, 2) - 2 * x + 2;
+
+        private void calculateBtn_Clicked(object sender, EventArgs e)
+        {
+            double xValue = 0.7d;
+
+            resultLbl.Text = $"Wynik: {CalculateQuadric(xValue)}";
+        }
+    }
+}
